46bb162378231da331da67e94c4b242323673344
EduardoBassani <88408579+EduardoBassani@users.noreply.github.com>
2022-04-04 15:06:21 -0300

    Add files via upload

---
+++ PT-PROv1-C43-Template-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Sempre mudando, não constante!", 100, 50);
+  text("dica: v r á v a i e v l",100,70)
+
+
+  fill("lightblue") 
+  text("Executa uma determinada tarefa", 100,160)
+  text("dica: u n ã o f ç",100,175)
+
+  fill("lightblue") 
+  text("Armazena todas as informações !!", 100,265)
+  text("b n a o c     e d    s d o a d",100,280)
+
+  text("lightblue")
+  text("PS:coloque em maiúsculo as resposta",100,450)
+
+  // Adicione código para exibir as outras duas perguntas.
+}
